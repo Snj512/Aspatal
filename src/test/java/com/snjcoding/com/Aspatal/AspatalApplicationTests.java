580af4a16ad4d575440d3013fde4d0de7ef402a4
Snehil Anand <snehilanand512@gmail.com>
2025-10-02 21:48:02 +0530

    New App

---
+++ src/test/java/com/snjcoding/com/Aspatal/AspatalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snjcoding.com.Aspatal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspatalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
