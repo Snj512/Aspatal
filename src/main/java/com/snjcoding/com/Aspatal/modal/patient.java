8b02a1c872cdc073cb8f99a2ab981d649f30b978
Snehil Anand <snehilanand512@gmail.com>
2025-10-05 17:36:48 +0530

    Change 1

---
+++ src/main/java/com/snjcoding/com/Aspatal/modal/patient.java
@@ -0,0 +1,16 @@
+package com.snjcoding.com.Aspatal.modal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+}
