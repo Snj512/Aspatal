5a29e5ec3dce678d54c2aa9ee9f9da5ad0005c91
Snehil Anand <snehilanand512@gmail.com>
2025-10-13 22:21:03 +0530

    Chnages 5

---
+++ src/main/java/com/snjcoding/com/Aspatal/modal/type/AuthProviderType.java
@@ -0,0 +1,9 @@
+package com.snjcoding.com.Aspatal.modal.type;
+
+public enum AuthProviderType {
+    GOOGLE,
+    GITHUB,
+    FACEBOOK,
+    TWITTER,
+    EMAIL
+}
---
+++ src/main/java/com/snjcoding/com/Aspatal/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.snjcoding.com.Aspatal.repository;
+
+import com.snjcoding.com.Aspatal.modal.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
